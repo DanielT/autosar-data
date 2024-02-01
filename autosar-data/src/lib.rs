DanielT/autosar-data

commit ec2119e4ef5bec8a039f33cfc9706988058b1496

allow comments to be attached to elements
Comments before the opening tag of an element are now considered to be attached to that element.
E.g.:
    <!--comment text-->
    <ELEMENT>...
Here "comment text" is stored as the attached comment of ELEMENT.

autosar-data/src/lib.rs
@@ -310,6 +310,7 @@ pub(crate) struct ElementRaw {
     pub(crate) content: SmallVec<[ElementContent; 4]>,
     pub(crate) attributes: SmallVec<[Attribute; 1]>,
     pub(crate) file_membership: HashSet<WeakArxmlFile>,
+    pub(crate) comment: Option<String>,
 }
 
 /// A single attribute of an arxml element

autosar-data/src/parser.rs
@@ -215,20 +215,29 @@ impl<'a> ArxmlParser<'a> {
             return Err(self.error(ArxmlParserError::InvalidArxmlFileHeader));
         }
 
-        if let ArxmlEvent::BeginElement(elemname, attributes_text) = self.next(&mut lexer)? {
+        let mut stored_comment = None;
+        let mut token = self.next(&mut lexer)?;
+        while let ArxmlEvent::Comment(comment_bytes) = token {
+            stored_comment = Some(String::from_utf8_lossy(comment_bytes).into());
+            token = self.next(&mut lexer)?;
+        }
+
+        if let ArxmlEvent::BeginElement(elemname, attributes_text) = token {
             if let Ok(ElementName::Autosar) = ElementName::from_bytes(elemname) {
                 let attributes = self.parse_attribute_text(ElementType::ROOT, attributes_text)?;
                 self.parse_file_header(&attributes)?;
 
-                let path = Cow::from("");
-                let autosar_root_element = self.parse_element(
-                    ElementOrModel::None,
-                    ElementName::Autosar,
+                let new_element = ElementRaw {
+                    parent: ElementOrModel::None,
+                    elemname: ElementName::Autosar,
+                    elemtype: ElementType::ROOT,
+                    content: SmallVec::new(),
                     attributes,
-                    ElementType::ROOT,
-                    path,
-                    &mut lexer,
-                )?;
+                    file_membership: HashSet::with_capacity(0),
+                    comment: stored_comment,
+                };
+                let path = Cow::from("");
+                let autosar_root_element = self.parse_element(new_element, path, &mut lexer)?;
                 self.verify_end_of_input(&mut lexer)?;
 
                 return Ok(autosar_root_element);
@@ -326,55 +335,45 @@ impl<'a> ArxmlParser<'a> {
     /// parse a single element of an arxml file
     fn parse_element(
         &mut self,
-        parent: ElementOrModel,
-        element_name: ElementName,
-        attributes: SmallVec<[Attribute; 1]>,
-        elemtype: ElementType,
+        raw_element: ElementRaw,
         mut path: Cow<str>,
         lexer: &mut ArxmlLexer,
     ) -> Result<Element, AutosarDataError> {
-        let wrapped_element = ElementRaw {
-            parent,
-            elemname: element_name,
-            attributes,
-            content: SmallVec::new(),
-            elemtype,
-            file_membership: HashSet::with_capacity(0),
-        }
-        .wrap();
+        let wrapped_element = raw_element.wrap();
         let mut element = wrapped_element.0.lock();
 
         let mut elem_idx: Vec<usize> = Vec::new();
         let mut short_name_found = false;
-        // let mut element_count = FxHashMap::<u16, usize>::default();
 
+        let mut stored_comment = None;
         loop {
             // track the current element name in the parser for error messages - set this in every loop iteration, since it gets overwritten during the recursive calls
-            self.current_element = element_name;
+            self.current_element = element.elemname;
             let arxmlevent = self.next(lexer)?;
             match arxmlevent {
                 ArxmlEvent::BeginElement(elem_text, attr_text) => {
                     if let Ok(name) = ElementName::from_bytes(elem_text) {
-                        let (sub_elemtype, idx) = self.find_element_in_spec_checked(name, elemtype)?;
-                        self.check_element_conflict(name, elemtype, &elem_idx, &idx)?;
+                        let (sub_elemtype, idx) = self.find_element_in_spec_checked(name, element.elemtype)?;
+                        self.check_element_conflict(name, element.elemtype, &elem_idx, &idx)?;
                         elem_idx = idx;
 
                         // make sure there aren't too many of this kind of element
                         if !element.content.is_empty() {
-                            self.check_multiplicity(name, elemtype, &elem_idx, &element)?;
+                            self.check_multiplicity(name, element.elemtype, &elem_idx, &element)?;
                         }
 
-                        // attributes for the sub-element must be parsed before calling parse_element, or else the borrow checker hates us
-                        let attributes = self.parse_attribute_text(sub_elemtype, attr_text)?;
                         // recursively parse the sub element and its sub sub elements
-                        let sub_element = self.parse_element(
-                            ElementOrModel::Element(wrapped_element.downgrade()),
-                            name,
-                            attributes,
-                            sub_elemtype,
-                            Cow::from(path.as_ref()),
-                            lexer,
-                        )?;
+                        let new_element = ElementRaw {
+                            parent: ElementOrModel::Element(wrapped_element.downgrade()),
+                            elemname: name,
+                            elemtype: sub_elemtype,
+                            content: SmallVec::new(),
+                            attributes: self.parse_attribute_text(sub_elemtype, attr_text)?,
+                            file_membership: HashSet::with_capacity(0),
+                            comment: stored_comment,
+                        };
+                        let sub_element = self.parse_element(new_element, Cow::from(path.as_ref()), lexer)?;
+                        stored_comment = None;
                         // if this sub element was a short name, then Autosar path handling is needed
                         if name == ElementName::ShortName {
                             short_name_found = true;
@@ -416,9 +415,9 @@ impl<'a> ArxmlParser<'a> {
                     }
                 }
                 ArxmlEvent::Characters(text_content) => {
-                    if let Some(character_data_spec) = elemtype.chardata_spec() {
+                    if let Some(character_data_spec) = element.elemtype.chardata_spec() {
                         let value = self.parse_character_data(text_content, character_data_spec)?;
-                        if elemtype.is_ref() {
+                        if element.elemtype.is_ref() {
                             if let CharacterData::String(refpath) = &value {
                                 self.references.push((refpath.to_owned(), wrapped_element.downgrade()));
                             }
@@ -438,13 +437,16 @@ impl<'a> ArxmlParser<'a> {
                         element: element.elemname,
                     }));
                 }
+                ArxmlEvent::Comment(comment_bytes) => {
+                    stored_comment = Some(String::from_utf8_lossy(comment_bytes).into());
+                }
             }
         }
 
         if short_name_found {
-        } else if elemtype.is_named_in_version(self.fileversion) {
+        } else if element.elemtype.is_named_in_version(self.fileversion) {
             self.optional_error(ArxmlParserError::RequiredSubelementMissing {
-                element: element_name,
+                element: element.elemname,
                 sub_element: ElementName::ShortName,
             })?;
         }

autosar-data/src/autosarmodel.rs
@@ -47,6 +47,7 @@ impl AutosarModel {
             content: SmallVec::new(),
             attributes: root_attributes,
             file_membership: HashSet::with_capacity(0),
+            comment: None,
         }
         .wrap();
         let model = AutosarModelRaw {

autosar-data/src/element.rs
@@ -1479,6 +1479,16 @@ impl Element {
     ) {
         let element_name = self.element_name().to_str();
 
+        if let Some(comment) = &self.0.lock().comment {
+            // put the comment on a separate line
+            if !inline {
+                self.serialize_newline_indent(outstring, indent);
+            }
+            outstring.push_str("<!--");
+            outstring.push_str(comment);
+            outstring.push_str("-->");
+        }
+
         // write the opening tag on a new line and indent it
         if !inline {
             self.serialize_newline_indent(outstring, indent);
@@ -1982,6 +1992,37 @@ impl Element {
         version: AutosarVersion,
     ) -> Result<(usize, usize), AutosarDataError> {
         self.0.lock().calc_element_insert_range(element_name, version)
+    }
+
+    /// Return the comment attachd to the element (if any)
+    ///
+    /// A comment directly preceding the opening tag is considered to be atached and is returned here.
+    ///
+    /// In the arxml text:
+    /// ```xml
+    ///     <!--element comment-->
+    ///     <ELEMENT> ...
+    /// ```
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use autosar_data::*;
+    /// # use std::collections::HashSet;
+    /// # let model = AutosarModel::new();
+    /// # let file = model.create_file("test", AutosarVersion::LATEST).unwrap();
+    /// # let element = model.root_element();
+    /// let opt_comment = element.comment();
+    /// ```
+    pub fn comment(&self) -> Option<String> {
+        self.0.lock().comment.clone()
+    }
+
+    /// Set or delete the comment attached to the element
+    ///
+    /// Set None to remove the comment.
+    pub fn set_comment(&self, opt_comment: Option<String>) {
+        self.0.lock().comment = opt_comment;
     }
 
     /// find the minumum version of all arxml files which contain this element

autosar-data/src/elementraw.rs
@@ -306,6 +306,7 @@ impl ElementRaw {
                 content: smallvec![],
                 attributes: smallvec![],
                 file_membership: HashSet::with_capacity(0),
+                comment: None,
             }
             .wrap();
             self.content
@@ -397,6 +398,7 @@ impl ElementRaw {
                 content: smallvec![],
                 attributes: smallvec![],
                 file_membership: HashSet::with_capacity(0),
+                comment: None,
             }
             .wrap();
             self.content
@@ -526,6 +528,7 @@ impl ElementRaw {
             attributes: SmallVec::with_capacity(self.attributes.len()),
             parent: ElementOrModel::None,
             file_membership: HashSet::with_capacity(0),
+            comment: self.comment.clone(),
         }
         .wrap();
 

autosar-data/src/iterators.rs
@@ -205,6 +205,7 @@ mod test {
             attributes: SmallVec::new(),
             content: SmallVec::new(),
             file_membership: HashSet::with_capacity(0),
+            comment: None,
         }
         .wrap();
         let sub_element = ElementRaw {
@@ -217,6 +218,7 @@ mod test {
                 ElementContent::Element(sub_sub_element.clone())
             ],
             file_membership: HashSet::with_capacity(0),
+            comment: None,
         }
         .wrap();
         let element = ElementRaw {
@@ -229,6 +231,7 @@ mod test {
                 ElementContent::Element(sub_element.clone())
             ],
             file_membership: HashSet::with_capacity(0),
+            comment: None,
         }
         .wrap();
         let dfs_iter = element.elements_dfs();

autosar-data/src/lexer.rs
@@ -28,6 +28,7 @@ pub(crate) enum ArxmlEvent<'a> {
     BeginElement(&'a [u8], &'a [u8]),
     EndElement(&'a [u8]),
     Characters(&'a [u8]),
+    Comment(&'a [u8]),
     EndOfFile,
 }
 
@@ -166,14 +167,15 @@ impl<'a> ArxmlLexer<'a> {
         result
     }
 
-    fn read_comment(&mut self, endpos: usize) -> Result<(), AutosarDataError> {
+    fn read_comment(&mut self, endpos: usize) -> Result<ArxmlEvent<'a>, AutosarDataError> {
         debug_assert!(self.bufpos < self.buffer.len());
         debug_assert!(endpos > self.bufpos + 1);
 
-        let text = &self.buffer[self.bufpos..endpos];
+        let startpos = self.bufpos;
+        let text = &self.buffer[startpos..endpos];
         self.bufpos = endpos + 1;
 
-        if !text.starts_with(b"<!--") || !text.ends_with(b"--") {
+        if text.len() < 6 || !text.starts_with(b"<!--") || !text.ends_with(b"--") {
             return Err(AutosarDataError::LexerError {
                 filename: self.sourcefile.clone(),
                 line: self.line,
@@ -181,7 +183,8 @@ impl<'a> ArxmlLexer<'a> {
             });
         }
         self.line += count_lines(text);
-        Ok(())
+        let comment = &self.buffer[startpos + 4..endpos - 2];
+        Ok(ArxmlEvent::Comment(comment))
     }
 }
 
@@ -224,8 +227,8 @@ impl<'a> ArxmlLexer<'a> {
                             }
                         }
                         b'!' => {
-                            // second char is '!' -> parse and ignore a comment
-                            self.read_comment(endpos)?;
+                            // second char is '!' -> parse a comment
+                            return self.read_comment(endpos).map(|res| (self.line, res));
                         }
                         _ => {
                             // any other second char -> BeginElement
@@ -240,7 +243,6 @@ impl<'a> ArxmlLexer<'a> {
                     }
                 }
                 // loop if:
-                // - a comment was ignored
                 // - a processing instruction was ignored
                 // - empty character data found (whitespace only)
             }
@@ -319,7 +321,7 @@ mod test {
     fn test_comment() {
         let data = b"<!-- foo--><element>";
         let mut lexer = ArxmlLexer::new(data, PathBuf::from("(buffer)"));
-        // the comment is skipped, and lexer.next() directly returns the following element
+        assert!(matches!(lexer.next(), Ok((_, ArxmlEvent::Comment(_)))));
         assert!(matches!(lexer.next(), Ok((_, ArxmlEvent::BeginElement(_elem, _attrs)))));
     }
 

commit aff1503b38ff0e8bb6f1fca0fb88671602305d97

fix check_file() / check_buffer() which were broken if the header contained a comment
this was broken by commit ec2119e: allow comments to be attached to elements

autosar-data/src/lib.rs
@@ -488,11 +488,14 @@ mod test {
 
         // file containing a valid arxml header -> true
         let header = r#"<?xml version="1.0" encoding="utf-8"?>
+        <!-- comment --><!-- comment 2 -->
         <AUTOSAR xsi:schemaLocation="http://autosar.org/schema/r4.0 AUTOSAR_00050.xsd" xmlns="http://autosar.org/schema/r4.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">"#;
         let arxml_file = dir.path().with_file_name("file.arxml");
         File::create(&arxml_file)
             .and_then(|mut file| file.write(header.as_bytes()))
             .unwrap();
         assert!(check_file(arxml_file));
+
+        assert!(check_buffer(header.as_bytes()));
     }
 }

autosar-data/src/parser.rs
@@ -830,7 +830,12 @@ impl<'a> ArxmlParser<'a> {
         let mut lexer = ArxmlLexer::new(self.buffer, self.filename.clone());
 
         if let Ok(ArxmlEvent::ArxmlHeader(_)) = self.next(&mut lexer) {
-            if let Ok(ArxmlEvent::BeginElement(elemname, attributes_text)) = self.next(&mut lexer) {
+            // skip any comments
+            let mut arxmlevent = self.next(&mut lexer);
+            while let Ok(ArxmlEvent::Comment(..)) = arxmlevent {
+                arxmlevent = self.next(&mut lexer);
+            }
+            if let Ok(ArxmlEvent::BeginElement(elemname, attributes_text)) = arxmlevent {
                 if let Ok(ElementName::Autosar) = ElementName::from_bytes(elemname) {
                     if let Ok(attributes) = self.parse_attribute_text(ElementType::ROOT, attributes_text) {
                         if self.parse_file_header(&attributes).is_ok() {