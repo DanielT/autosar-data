2f7bcda3e9d336e681d402669077c70dcd673551
Daniel Thaler <daniel@dthaler.de>
2024-09-25 20:34:46 +0200

    Support ports, port interfaces, data mappings

---
+++ autosar-data-abstraction/src/software_component/interface/clientserver.rs
@@ -0,0 +1,173 @@
+use crate::*;
+use autosar_data::ElementName;
+use datatype::AutosarDataType;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct ClientServerInterface(pub(crate) Element);
+abstraction_element!(ClientServerInterface, ClientServerInterface);
+
+impl ClientServerInterface {
+    /// Create a new ClientServerInterface
+    pub fn new(name: &str, package: &ArPackage) -> Result<Self, AutosarAbstractionError> {
+        let elements = package.element().get_or_create_sub_element(ElementName::Elements)?;
+        let client_server_interface = elements.create_named_sub_element(ElementName::ClientServerInterface, name)?;
+
+        Ok(Self(client_server_interface))
+    }
+
+    /// Add a possible error to the client server interface
+    pub fn add_possible_error(&self, name: &str, error_code: u64) -> Result<ApplicationError, AutosarAbstractionError> {
+        let possible_errors = self.element().get_or_create_sub_element(ElementName::PossibleErrors)?;
+        ApplicationError::new(name, error_code, &possible_errors)
+    }
+
+    /// add an operation to the client server interface
+    pub fn add_operation(&self, name: &str) -> Result<ClientServerOperation, AutosarAbstractionError> {
+        let operations = self.element().get_or_create_sub_element(ElementName::Operations)?;
+        ClientServerOperation::new(name, &operations)
+    }
+
+    /// iterate over all operations
+    pub fn operations(&self) -> ClientServerOperationIterator {
+        ClientServerOperationIterator::new(self.element().get_sub_element(ElementName::Operations))
+    }
+
+    /// iterate over all application errors
+    pub fn possible_errors(&self) -> ApplicationErrorIterator {
+        ApplicationErrorIterator::new(self.element().get_sub_element(ElementName::PossibleErrors))
+    }
+}
+
+//##################################################################
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct ApplicationError(Element);
+abstraction_element!(ApplicationError, ApplicationError);
+
+impl ApplicationError {
+    /// Create a new ApplicationError
+    fn new(name: &str, error_code: u64, parent_element: &Element) -> Result<Self, AutosarAbstractionError> {
+        let application_error = parent_element.create_named_sub_element(ElementName::ApplicationError, name)?;
+        application_error
+            .create_sub_element(ElementName::ErrorCode)?
+            .set_character_data(error_code)?;
+        Ok(Self(application_error))
+    }
+}
+
+//##################################################################
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct ClientServerOperation(Element);
+abstraction_element!(ClientServerOperation, ClientServerOperation);
+
+impl ClientServerOperation {
+    /// Create a new ClientServerOperation
+    fn new(name: &str, parent_element: &Element) -> Result<Self, AutosarAbstractionError> {
+        let operation = parent_element.create_named_sub_element(ElementName::ClientServerOperation, name)?;
+        Ok(Self(operation))
+    }
+
+    /// Add an argument to the operation
+    pub fn add_argument(
+        &self,
+        name: &str,
+        data_type: &AutosarDataType,
+        direction: ArgumentDirection,
+    ) -> Result<ArgumentDataPrototype, AutosarAbstractionError> {
+        let arguments = self.element().get_or_create_sub_element(ElementName::Arguments)?;
+        ArgumentDataPrototype::new(name, &arguments, data_type.element(), direction)
+    }
+
+    /// add a reference to possible error to the operation
+    pub fn add_possible_error_reference(&self, error: &ApplicationError) -> Result<(), AutosarAbstractionError> {
+        if self.element().named_parent()? != error.element().named_parent()? {
+            return Err(AutosarAbstractionError::InvalidParameter(
+                "Error and operation must be in the same ClientServerInterface".to_string(),
+            ));
+        }
+
+        let possible_errors = self.element().get_or_create_sub_element(ElementName::PossibleErrors)?;
+        possible_errors
+            .create_sub_element(ElementName::PossibleErrorRef)?
+            .set_reference_target(error.element())?;
+        Ok(())
+    }
+
+    /// iterate over all arguments
+    pub fn arguments(&self) -> impl Iterator<Item = ArgumentDataPrototype> {
+        ArgumentDataPrototypeIterator::new(self.element().get_sub_element(ElementName::Arguments))
+    }
+}
+
+//##################################################################
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ArgumentDirection {
+    In,
+    Out,
+    InOut,
+}
+
+impl TryFrom<EnumItem> for ArgumentDirection {
+    type Error = AutosarAbstractionError;
+
+    fn try_from(item: EnumItem) -> Result<Self, Self::Error> {
+        match item {
+            EnumItem::In => Ok(ArgumentDirection::In),
+            EnumItem::Out => Ok(ArgumentDirection::Out),
+            EnumItem::Inout => Ok(ArgumentDirection::InOut),
+            _ => Err(AutosarAbstractionError::ValueConversionError {
+                value: item.to_string(),
+                dest: "ArgumentDirection".to_string(),
+            }),
+        }
+    }
+}
+
+impl From<ArgumentDirection> for EnumItem {
+    fn from(direction: ArgumentDirection) -> Self {
+        match direction {
+            ArgumentDirection::In => EnumItem::In,
+            ArgumentDirection::Out => EnumItem::Out,
+            ArgumentDirection::InOut => EnumItem::Inout,
+        }
+    }
+}
+
+//##################################################################
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct ArgumentDataPrototype(Element);
+abstraction_element!(ArgumentDataPrototype, ArgumentDataPrototype);
+
+impl ArgumentDataPrototype {
+    /// Create a new ArgumentDataPrototype
+    fn new(
+        name: &str,
+        parent_element: &Element,
+        data_type: &Element,
+        direction: ArgumentDirection,
+    ) -> Result<Self, AutosarAbstractionError> {
+        let argument = parent_element.create_named_sub_element(ElementName::ArgumentDataPrototype, name)?;
+        argument
+            .create_sub_element(ElementName::TypeTref)?
+            .set_reference_target(data_type)?;
+        argument
+            .create_sub_element(ElementName::Direction)?
+            .set_character_data::<EnumItem>(direction.into())?;
+        Ok(Self(argument))
+    }
+}
+
+//##################################################################
+
+element_iterator!(ClientServerOperationIterator, ClientServerOperation, Some);
+
+//##################################################################
+
+element_iterator!(ApplicationErrorIterator, ApplicationError, Some);
+
+//##################################################################
+
+element_iterator!(ArgumentDataPrototypeIterator, ArgumentDataPrototype, Some);
--- autosar-data-abstraction/src/system/mod.rs
+++ autosar-data-abstraction/src/system/mod.rs
@@ -4,13 +4,14 @@ use crate::communication::{
     MultiplexedIPdu, NPdu, NmPdu, SecuredIPdu, SystemSignal, SystemSignalGroup,
 };
 use crate::datatype::SwBaseType;
-use crate::software_component::{
-    AbstractSwComponentType, ComponentPrototype, CompositionSwComponentType, RootSwCompositionPrototype,
-    SwComponentPrototype,
-};
+use crate::software_component::{CompositionSwComponentType, RootSwCompositionPrototype};
 use crate::{abstraction_element, AbstractionElement, ArPackage, AutosarAbstractionError, EcuInstance};
 use autosar_data::{AutosarDataError, AutosarModel, Element, ElementName, WeakElement};
 use std::iter::FusedIterator;
+
+mod mapping;
+
+pub use mapping::*;
 
 /// The System is the top level of a system template
 ///
@@ -709,147 +710,6 @@ impl System {
             }
         }
         SystemMapping::new(name, self)
-    }
-}
-
-//#########################################################
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct SystemMapping(Element);
-abstraction_element!(SystemMapping, SystemMapping);
-
-impl SystemMapping {
-    pub(crate) fn new(name: &str, system: &System) -> Result<Self, AutosarAbstractionError> {
-        let element = system
-            .element()
-            .get_or_create_sub_element(ElementName::Mappings)?
-            .create_named_sub_element(ElementName::SystemMapping, name)?;
-
-        Ok(Self(element))
-    }
-
-    /// get the system that contains this mapping
-    pub fn system(&self) -> Result<System, AutosarAbstractionError> {
-        let sys_elem = self.element().named_parent()?.unwrap();
-        System::try_from(sys_elem)
-    }
-
-    /// create a new mapping between a SWC and an ECU
-    pub fn map_swc_to_ecu(
-        &self,
-        name: &str,
-        component_prototype: &SwComponentPrototype,
-        ecu: &EcuInstance,
-    ) -> Result<SwcToEcuMapping, AutosarAbstractionError> {
-        let root_composition_prototype =
-            self.system()?
-                .root_composition()
-                .ok_or(AutosarAbstractionError::InvalidParameter(
-                    "The root compositon must be set before mapping any swc".to_string(),
-                ))?;
-        let root_composition_type =
-            root_composition_prototype
-                .composition()
-                .ok_or(AutosarAbstractionError::InvalidParameter(
-                    "Incomplete root composition prototype".to_string(),
-                ))?;
-
-        let mut context_composition_prototypes = vec![];
-        let mut current_composition = component_prototype.parent_composition()?;
-
-        // check if the composition is a child of the root composition; this is needed to ensure that the loop can terminate
-        if root_composition_type != current_composition && !root_composition_type.is_parent_of(&current_composition) {
-            return Err(AutosarAbstractionError::InvalidParameter(
-                "The composition is not a child of the root composition".to_string(),
-            ));
-        }
-
-        // find all compositions between the root composition and the current composition
-        while current_composition != root_composition_type {
-            // typical case is that each component is only in one composition, so the for loop should only run once
-            for comp_proto in current_composition.instances() {
-                // this condition should never fail - it only returns none if comp_proto is the root
-                // composition, which we already know is not true
-                if let Ok(Some(comp_type)) = comp_proto.parent_composition() {
-                    if root_composition_type == comp_type || root_composition_type.is_parent_of(&comp_type) {
-                        context_composition_prototypes.push(comp_proto.clone());
-                        current_composition = comp_type;
-                        break;
-                    }
-                }
-            }
-        }
-
-        // the items were collected in reverse order, so we need to reverse them again
-        context_composition_prototypes.reverse();
-
-        SwcToEcuMapping::new(
-            name,
-            component_prototype,
-            &context_composition_prototypes,
-            &root_composition_prototype,
-            ecu,
-            self,
-        )
-    }
-}
-
-//#########################################################
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct SwcToEcuMapping(Element);
-abstraction_element!(SwcToEcuMapping, SwcToEcuMapping);
-
-impl SwcToEcuMapping {
-    pub(crate) fn new(
-        name: &str,
-        component_prototype: &SwComponentPrototype,
-        context_composition_prototypes: &[ComponentPrototype],
-        root_composition_prototype: &RootSwCompositionPrototype,
-        ecu: &EcuInstance,
-        mapping: &SystemMapping,
-    ) -> Result<Self, AutosarAbstractionError> {
-        let sw_mappings_elem = mapping.element().get_or_create_sub_element(ElementName::SwMappings)?;
-        let swc_to_ecu_mapping = sw_mappings_elem.create_named_sub_element(ElementName::SwcToEcuMapping, name)?;
-
-        let iref = swc_to_ecu_mapping
-            .create_sub_element(ElementName::ComponentIrefs)?
-            .create_sub_element(ElementName::ComponentIref)?;
-
-        // create the references to root composition and context compositions
-        iref.create_sub_element(ElementName::ContextCompositionRef)?
-            .set_reference_target(root_composition_prototype.element())?;
-        for context_comp in context_composition_prototypes {
-            iref.create_sub_element(ElementName::ContextComponentRef)?
-                .set_reference_target(context_comp.element())?;
-        }
-        // create the reference to the target component prototype
-        iref.create_sub_element(ElementName::TargetComponentRef)?
-            .set_reference_target(component_prototype.element())?;
-
-        swc_to_ecu_mapping
-            .create_sub_element(ElementName::EcuInstanceRef)?
-            .set_reference_target(ecu.element())?;
-
-        Ok(Self(swc_to_ecu_mapping))
-    }
-
-    /// get the component prototype that is mapped here
-    pub fn target_component(&self) -> Option<SwComponentPrototype> {
-        self.element()
-            .get_sub_element(ElementName::ComponentIrefs)
-            .and_then(|irefs| irefs.get_sub_element(ElementName::ComponentIref))
-            .and_then(|iref| iref.get_sub_element(ElementName::TargetComponentRef))
-            .and_then(|target| target.get_reference_target().ok())
-            .and_then(|target| SwComponentPrototype::try_from(target).ok())
-    }
-
-    /// get the ECU instance which is the target of this mapping
-    pub fn ecu_instance(&self) -> Option<EcuInstance> {
-        self.element()
-            .get_sub_element(ElementName::EcuInstanceRef)
-            .and_then(|r| r.get_reference_target().ok())
-            .and_then(|target| EcuInstance::try_from(target).ok())
     }
 }
 
